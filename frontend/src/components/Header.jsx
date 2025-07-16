5b5361ae45c2580326bd60e31ba85db04e4758ee
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-07-16 23:55:25 +0530

    DOne

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Header.scss'
 import MoodOverlayPlayer from './MoodOverlayPlayer.jsx'
+import Typewriter from "typewriter-effect";
 const Header = () => {
   return (
     <>
-    <h1>Live Mood Detection</h1>
+      <h1 className='headerTitle'>
+        <Typewriter
+          options={{
+            strings: [
+              "Welcome to Moody Player",
+              "Your Mood, Your Music",
+              "Feel the Beat of Your Emotions"
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h1>
+
     <div className='headerrr'>
         <div className="headerLft">
             <MoodOverlayPlayer />
@@ -12,6 +26,7 @@ const Header = () => {
         <div className="headerRgt">
             <h4>Moody Player</h4>
             <p>Your Current mood is being analyzed in <br /> real time . Enjoy music Tailored to your feelings</p>
+            <button>Start Detecting...</button>
         </div>
     </div>
     </>
