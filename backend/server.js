51be18b4db52910da4b5c0d5f5b6279af4ef391c
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-09-16 15:12:57 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/', songRoutes);
 
+app.get('/', (req, res) => {
+    res.send("Welcome to Moodify AI Music Player Backend");
+});
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
